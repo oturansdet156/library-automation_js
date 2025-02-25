42d33762ccb4f23eb8615afbfc8e9572acbc7344
orhanturan3898156 <orhanturan1566@gmail.com>
2025-02-25 12:35:13 -0500

    book categories feature

--- pages/DashboardPage.js
+++ pages/DashboardPage.js
@@ -1,14 +1,20 @@
 import { BasePage } from "./BasePage.js";
 
 export class DashboardPage extends BasePage {
+
   // ADD YOUR LOCATORS HERE...
-  /**
+
+   /**
    * @param {import('playwright').Page} page
    */
-  constructor(page) {
+   constructor(page) {
     super(page);
-    this.userProfileImage = page.locator("//img[@id='user_avatar']");
-    this.logoutButton = page.getByText('Log Out');  
+    this.userprofileImage = page.locator("//img[@id='user_avatar']");
+    this.logoutButton = page.getByText('Log Out');
+    this.bookLink = page.locator("//a[@href='#books' and @class='nav-link']");
+   
+    
   }
+
 
 }
